8cbd1fafa0d72c3579c6ab8d3a0321940fffd035
MariaCamilaChac <32178494+MariaCamilaChac@users.noreply.github.com>
2019-03-13 16:19:35 -0500

    Update Juego.java

--- Juego.java
+++ Juego.java
@@ -2,20 +2,36 @@ import java.util.ArrayList;
 import java.util.List;
 import com.modeliosoft.modelio.javadesigner.annotations.objid;
 
-@objid ("79081a31-1308-44c6-a005-9c78fca9add3")
 public class Juego {
-    @objid ("b3a175c2-db3a-4b86-9f5e-c9ab4999f961")
+  
     private boolean juegoTerminado;
+    
+    public static boolean isJuegoTerminado(boolean juegoTerminado){
+    }
 
-    @objid ("72ca422c-b97c-4ae0-97ba-c6054281b069")
     public List<Tetromino> tetromino = new ArrayList<Tetromino> ();
 
-    @objid ("91cce22c-e8ad-460f-abf8-a79f49db53c7")
     public static void Juego(Coordenada ) {
     }
-
-    @objid ("13bc3f70-4192-4b8b-9a86-5e209cf007e1")
-    public synchronized void getTablero() {
+    
+    public void rotarDerecha(int matriz[][]){
     }
+    
+    public void rotarIzquierda(int matriz[][]){
+    }
+    
+    public void desplazarIzquierda(Coordenada){
+    }
+    
+    public void desplazarDerecha(Coordenada){
+    }
+    
+    public void static boolean fichaTerminada(boolean fichaTerminada){
+    }
+    
+    
+    
+
+
 
 }
