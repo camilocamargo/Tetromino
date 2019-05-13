da75e5fa1ebf11fc1654d4e3d35c83f9554f9633
MariaCamilaChac <32178494+MariaCamilaChac@users.noreply.github.com>
2019-05-13 10:23:18 -0500

    Update Coordenada.java

--- Coordenada.java
+++ Coordenada.java
@@ -6,13 +6,16 @@ public class Coordenada {
     private int columnaInicio;
     private int columnaFin;
 
-    public Coordenada(int fila, int columna) {
-        this.fila = fila;
-        this.columna = columna;
+    public Coordenada(int filaInicio, int filaFin, int columnaInicio, int columnaFin) {
+        this.filaInicio = filaInicio;
+		this.filaFin = filaFin;
+        this.columnaInicio = columnaInicio;
+		this.columnaFin = columnaFin;
     }
 
 
     public int getCoordena() {
-        
+        int [] coordenada = {this.filaInicio, this.filaFin, this.columnaInicio, this.columnaFin};
+		return; 
     }
 }
